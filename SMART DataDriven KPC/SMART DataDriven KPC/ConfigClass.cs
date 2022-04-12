0d286f9aa2dc0fed9658288d9c12d9e16607557e
Павел Велетнюк <mrkripster4@gmail.com>
2022-04-12 20:00:14 +0300

    Add DDT

--- SMART DataDriven KPC/SMART DataDriven KPC/ConfigClass.cs
+++ SMART DataDriven KPC/SMART DataDriven KPC/ConfigClass.cs
@@ -1,7 +1,11 @@
-﻿namespace SMART_DataDriven_KPC
+﻿using System.IO;
+
+namespace SMART_DataDriven_KPC
 {
     public static class ConfigClass
     {
         public const string MainUrl = "https://my.kaspersky.com/";
+        public static readonly string DefaultPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public static readonly string TestMails = DefaultPath + @"\Resources\TestMails.xlsx";
     }
 }
--- SMART DataDriven KPC/SMART DataDriven KPC/KPCTest.cs
+++ SMART DataDriven KPC/SMART DataDriven KPC/KPCTest.cs
@@ -1,5 +1,9 @@
+using Ganss.Excel;
 using NUnit.Framework;
+using SMART_DataDriven_KPC.Models;
+using SMART_DataDriven_KPC.Pages;
 using SMART_DataDriven_KPC.Test_conditions;
+using System.Collections.Generic;
 
 namespace SMART_DataDriven_KPC
 {
@@ -7,9 +11,28 @@ namespace SMART_DataDriven_KPC
     {
 
         [Test]
-        public void Test1()
+        [TestCaseSource("TestMails")]
+        public void Test1(UserAccountModel model)
         {
-            Assert.Pass();
+            SignInPage signIn = new SignInPage();
+            HeaderMenu headerMenu = new HeaderMenu();
+            DownloadsPage downloadsPage = new DownloadsPage();
+
+            signIn.SignIn(model);
+            Assert.AreEqual(model.UserName, headerMenu.GetUserName(), "User nme are not equal");
+            headerMenu.GoToDownloads();
+            downloadsPage.PrepareToSendSelf(model);
+            downloadsPage.GetEmailFromSendSelf();
+            downloadsPage.SendSelf();
+        }
+
+        public static IEnumerable<UserAccountModel> TestMails()
+        {
+            var Mails = new ExcelMapper(ConfigClass.TestMails).Fetch<UserAccountModel>();
+            foreach (var mail in Mails)
+            {
+                yield return mail;
+            }
         }
     }
 }
