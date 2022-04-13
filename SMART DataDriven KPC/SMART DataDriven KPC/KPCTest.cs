7a71f5be7ce50c53609f7b15bd1856524a1aa885
Павел Велетнюк <mrkripster4@gmail.com>
2022-04-13 17:34:41 +0300

    Task almost done

--- SMART DataDriven KPC/SMART DataDriven KPC/KPCTest.cs
+++ SMART DataDriven KPC/SMART DataDriven KPC/KPCTest.cs
@@ -1,9 +1,12 @@
+using AE.Net.Mail;
+using Aquality.Selenium.Browsers;
 using Ganss.Excel;
 using NUnit.Framework;
 using SMART_DataDriven_KPC.Models;
 using SMART_DataDriven_KPC.Pages;
 using SMART_DataDriven_KPC.Test_conditions;
 using System.Collections.Generic;
+using System.Text;
 
 namespace SMART_DataDriven_KPC
 {
@@ -18,12 +21,16 @@ namespace SMART_DataDriven_KPC
             HeaderMenu headerMenu = new HeaderMenu();
             DownloadsPage downloadsPage = new DownloadsPage();
 
+            Assert.IsTrue(signIn.State.IsDisplayed, "Sign In page is not open");
             signIn.SignIn(model);
-            Assert.AreEqual(model.UserName, headerMenu.GetUserName(), "User nme are not equal");
+            Assert.IsTrue(AqualityServices.ConditionalWait.WaitFor(() => headerMenu.State.IsDisplayed), "Header is not load");
+            Assert.AreEqual(model.UserName, headerMenu.GetUserName(), "User name are not equal");
             headerMenu.GoToDownloads();
+            Assert.IsTrue(AqualityServices.ConditionalWait.WaitFor(() => downloadsPage.State.IsDisplayed), "Downloads page is not open");
             downloadsPage.PrepareToSendSelf(model);
             downloadsPage.GetEmailFromSendSelf();
             downloadsPage.SendSelf();
+            Assert.IsTrue(AqualityServices.ConditionalWait.WaitFor(() => EmailCheck(model)), "Didn't receive an email with a valid link");
         }
 
         public static IEnumerable<UserAccountModel> TestMails()
@@ -33,6 +40,15 @@ namespace SMART_DataDriven_KPC
             {
                 yield return mail;
             }
+        }
+
+        public static bool EmailCheck(UserAccountModel model)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ImapClient IC = new ImapClient("imap.gmail.com", model.UserName, model.Password, AuthMethods.Login, 993, true);
+            IC.SelectMailbox("INBOX");
+            var Email = IC.GetMessage(IC.GetMessageCount() - 1);
+            return Email.Body.Contains(model.Product) && Email.Body.Contains("https://my.kaspersky.com/MyLicenses?startDownload=https");
         }
     }
 }
