5be5efa35509d7d104bb06e4d85e04fac4debab1
Павел Велетнюк <mrkripster4@gmail.com>
2022-04-29 16:25:56 +0300

    Task done

--- DBTests/DBTests/UnitTest1.cs
+++ DBTests/DBTests/UnitTest1.cs
@@ -1,4 +1,4 @@
-using DBTests.DBRequests;
+using Aquality.Selenium.Browsers;
 using DBTests.Models;
 using DBTests.Test_conditions;
 using DBTests.Utils;
@@ -14,6 +14,8 @@ namespace DBTests
         public void MinWorkingTime()
         {
             var MinWorkingTimeResp = dBRequests.GetMinWorkingTime();
+            DBLogUtil.MinWorkingLog(MinWorkingTimeResp);
+            AqualityServices.Logger.Info($"Getting test data from {ConfigClass.MinTimeTestPath}");
             var MinWorkingTime = ParseJSON.GetDataFile<List<MinTimeTestModel>>(ConfigClass.MinTimeTestPath);
             Assert.IsTrue(CompareUtil.IsListsAreEqual(MinWorkingTimeResp, MinWorkingTime), "The minimum test execution time does not match the test data");
         }
@@ -22,6 +24,8 @@ namespace DBTests
         public void NumberOfUniqueTests()
         {
             var NumberOfUniqueResp = dBRequests.GetNumberOfUniqueTests();
+            DBLogUtil.NumberOfUniqueLog(NumberOfUniqueResp);
+            AqualityServices.Logger.Info($"Getting test data from {ConfigClass.TestCountPath}");
             var NumberOfUnique = ParseJSON.GetDataFile<List<NumberOfUniqueTests>>(ConfigClass.TestCountPath);
             Assert.IsTrue(CompareUtil.IsListsAreEqual(NumberOfUniqueResp, NumberOfUnique), "The number of unique tests on the projects does not match the test data");
         }
@@ -29,7 +33,9 @@ namespace DBTests
         [Test]
         public void DateConditionTests()
         {
-            var DateConditionResp=dBRequests.GetDateConditionTests();
+            var DateConditionResp = dBRequests.GetDateConditionTests();
+            DBLogUtil.DateConditionLog(DateConditionResp);
+            AqualityServices.Logger.Info($"Getting test data from {ConfigClass.DateCondTestPath}");
             var DateCondition = ParseJSON.GetDataFile<List<DateConditionModel>>(ConfigClass.DateCondTestPath);
             Assert.IsTrue(CompareUtil.IsListsAreEqual(DateConditionResp, DateCondition), "The date conditions tests does not match the test data");
         }
@@ -38,6 +44,8 @@ namespace DBTests
         public void BrowserCountTests()
         {
             var BrowserCountResp = dBRequests.GetBrowserCountTests();
+            DBLogUtil.BrowserCountLog(BrowserCountResp);
+            AqualityServices.Logger.Info($"Getting test data from {ConfigClass.BrowserCountTestPath}");
             var BrowserCount = ParseJSON.GetDataFile<List<BrowserCountTestModel>>(ConfigClass.BrowserCountTestPath);
             Assert.IsTrue(CompareUtil.IsListsAreEqual(BrowserCountResp, BrowserCount), "The browsers count tests does not match the test data");
         }
---
+++ DBTests/DBTests/Utils/DBLogUtil.cs
@@ -0,0 +1,49 @@
+﻿using Aquality.Selenium.Browsers;
+using DBTests.Models;
+using System.Collections.Generic;
+
+namespace DBTests.Utils
+{
+    public static class DBLogUtil
+    {
+        public static void MinWorkingLog(List<MinTimeTestModel> list)
+        {
+            AqualityServices.Logger.Info($"The minimum test execution time data table");
+            AqualityServices.Logger.Info($"Project\t|Test\t|MinTime\t|");
+            foreach (var test in list)
+            {
+                AqualityServices.Logger.Info($"{test.Project}\t|{test.Test}\t|{test.MinTime}\t|");
+            }
+        }
+
+        public static void NumberOfUniqueLog(List<NumberOfUniqueTests> list)
+        {
+            AqualityServices.Logger.Info($"The number of unique tests on the projects data table");
+            AqualityServices.Logger.Info($"Project\t|TestsCount\t|");
+            foreach (var test in list)
+            {
+                AqualityServices.Logger.Info($"{test.Project}\t|{test.TestsCount}\t|");
+            }
+        }
+
+        public static void DateConditionLog(List<DateConditionModel> list)
+        {
+            AqualityServices.Logger.Info($"The date conditions tests data table");
+            AqualityServices.Logger.Info($"Project\t|Test\t|Date\t|");
+            foreach (var test in list)
+            {
+                AqualityServices.Logger.Info($"{test.Project}\t|{test.Test}\t|{test.Date}\t|");
+            }
+        }
+
+        public static void BrowserCountLog(List<BrowserCountTestModel> list)
+        {
+            AqualityServices.Logger.Info($"The browsers count tests data table");
+            AqualityServices.Logger.Info($"Brosers\t|");
+            foreach (var test in list)
+            {
+                AqualityServices.Logger.Info($"{test.Brosers}\t|");
+            }
+        }
+    }
+}
